7572f84d3e29928d5a19dc303f8f594fce3cc49b
unaussprechlich@pc <keck.alexander@web.de>
2017-12-25 20:31:03 +0100

    ;)

--- src/main/java/net/unaussprechlich/mvpplusplusfix/MvpPlusPlusFix.java
+++ src/main/java/net/unaussprechlich/mvpplusplusfix/MvpPlusPlusFix.java
@@ -7,31 +7,33 @@ import net.minecraft.util.IChatComponent;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-@SideOnly(Side.CLIENT)
-@Mod(modid = MvpPlusPlusFix.MODID, version = MvpPlusPlusFix.VERSION, acceptedMinecraftVersions = "1.8.9")
+@Mod(modid = MvpPlusPlusFix.MODID, version = MvpPlusPlusFix.VERSION, acceptedMinecraftVersions = "1.8.9", clientSideOnly = true)
 public class MvpPlusPlusFix {
 
-    static final String MODID = "mvpppfix";
+    static final String MODID = "mvpplusplusfix";
     static final String VERSION = "0.1";
 
     private static final String MVPppREGEX = ".*(§r)?§6\\[MVP(§r)?§[0-9|a-f]\\+\\+(§r)?§6].*";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
+        FMLCommonHandler.instance().bus().register(this);
         MinecraftForge.EVENT_BUS.register(this);
+        FMLLog.info("", "[MVP++]-Fix loaded.");
     }
 
-    @SubscribeEvent
+    @SubscribeEvent(receiveCanceled = true, priority = EventPriority.HIGH)
     public void onChatMessage(ClientChatReceivedEvent e) {
         try {
+            System.out.println("TEST");
             System.out.println(e.message.getFormattedText().matches(MVPppREGEX) || e.message.getUnformattedText().matches(MVPppREGEX));
             if(e.message.getFormattedText().matches(MVPppREGEX) || e.message.getUnformattedText().matches(MVPppREGEX)){
 
@@ -71,13 +73,15 @@ public class MvpPlusPlusFix {
         }
     }
 
-
     @SubscribeEvent
     public void onPlayerName(PlayerEvent.NameFormat e) {
         try{
             ScorePlayerTeam team = (ScorePlayerTeam) e.entityPlayer.getTeam();
-            if(team.getColorPrefix().matches(MVPppREGEX))
+            if(team.getColorPrefix().matches(MVPppREGEX)){
                 team.setNamePrefix("§b[MVP§" + team.getColorPrefix().charAt(7) + "++§b]");
+                e.entityPlayer.refreshDisplayName();
+            }
+
         } catch(Exception ex){
             ex.printStackTrace();
         }
