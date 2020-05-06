f4ad862a9a7cfc7275c034d150056ba2e9f18d88
WM <34175525+ebicep@users.noreply.github.com>
2020-05-06 18:14:00 -0400

    change to kotlin

--- src/main/java/net/unaussprechlich/warlordsplus/hud/elements/HudElementDamageAndHealingCounter.kt
+++ src/main/java/net/unaussprechlich/warlordsplus/hud/elements/HudElementDamageAndHealingCounter.kt
@@ -1,70 +1,118 @@
-package net.unaussprechlich.warlordsplus.hud.elements;
+package net.unaussprechlich.warlordsplus.hud.elements
 
-import net.minecraft.util.EnumChatFormatting;
-import net.minecraftforge.event.world.NoteBlockEvent;
-import net.unaussprechlich.warlordsplus.Player;
-import net.unaussprechlich.warlordsplus.module.modules.GameStateManager;
-import net.unaussprechlich.warlordsplus.module.modules.ScoreboardManager;
-import net.unaussprechlich.warlordsplus.config.CCategory;
-import net.unaussprechlich.warlordsplus.config.ConfigPropertyBoolean;
-import net.unaussprechlich.warlordsplus.hud.AbstractHudElement;
+import net.minecraft.util.EnumChatFormatting
+import net.unaussprechlich.warlordsplus.Player
+import net.unaussprechlich.warlordsplus.Player.damageDoneCounter
+import net.unaussprechlich.warlordsplus.Player.damageTakenCounter
+import net.unaussprechlich.warlordsplus.Player.energyGivenCounter
+import net.unaussprechlich.warlordsplus.Player.energyLostCounter
+import net.unaussprechlich.warlordsplus.Player.energyReceivedCounter
+import net.unaussprechlich.warlordsplus.Player.energyStolenCounter
+import net.unaussprechlich.warlordsplus.Player.healingGivenCounter
+import net.unaussprechlich.warlordsplus.Player.healingReceivedCounter
+import net.unaussprechlich.warlordsplus.Player.specialization
+import net.unaussprechlich.warlordsplus.config.CCategory
+import net.unaussprechlich.warlordsplus.config.ConfigPropertyBoolean
+import net.unaussprechlich.warlordsplus.hud.AbstractHudElement
+import net.unaussprechlich.warlordsplus.module.modules.GameStateManager.isIngame
+import java.util.*
 
-import java.util.ArrayList;
-
-public class HudElementDamageAndHealingCounter extends AbstractHudElement {
-
-    @ConfigPropertyBoolean(category = CCategory.HUD, id = "showHealingDone", comment = "Enable or disable the Healing Done counter", def = true)
-    public static boolean showHealingDone = false;
-    @ConfigPropertyBoolean(category = CCategory.HUD, id = "showDamageDone", comment = "Enable or disable the Damage Done counter", def = true)
-    public static boolean showDamageDone = false;
-    @ConfigPropertyBoolean(category = CCategory.HUD, id = "showEnergyReceived", comment = "Enable or disable the Energy Received counter", def = true)
-    public static boolean showEnergyReceived = false;
-    @ConfigPropertyBoolean(category = CCategory.HUD, id = "showHealingReceived", comment = "Enable or disable the Healing Received counter", def = true)
-    public static boolean showHealingReceived = false;
-    @ConfigPropertyBoolean(category = CCategory.HUD, id = "showDamageTaken", comment = "Enable or disable the Damage Taken counter", def = true)
-    public static boolean showDamageTaken = false;
-    @ConfigPropertyBoolean(category = CCategory.HUD, id = "showEnergyGiven", comment = "Enable or disable the Energy Given counter", def = true)
-    public static boolean showEnergyGiven = false;
-    @ConfigPropertyBoolean(category = CCategory.HUD, id = "showEnergyStolen", comment = "Enable or disable the Energy Stolen counter", def = true)
-    public static boolean showEnergyStolen = false;
-    @ConfigPropertyBoolean(category = CCategory.HUD, id = "showEnergyLost", comment = "Enable or disable the Energy Lost counter", def = true)
-    public static boolean showEnergyLost = false;
-
-    @Override
-    public String[] getRenderString() {
+class HudElementDamageAndHealingCounter : AbstractHudElement() {
+    override fun getRenderString(): Array<String> {
 
         //Todo add all the values
-
-        ArrayList<String> renderStrings = new ArrayList<>();
-
+        val renderStrings = ArrayList<String>()
         if (showDamageDone)
-            renderStrings.add(EnumChatFormatting.RED + "Damage: " + Player.INSTANCE.getDamageDoneCounter());
+            renderStrings.add(EnumChatFormatting.RED.toString() + "Damage: " + damageDoneCounter)
         if (showHealingDone)
-            renderStrings.add(EnumChatFormatting.GREEN + "Healing: " + Player.INSTANCE.getHealingGivenCounter());
+            renderStrings.add(EnumChatFormatting.GREEN.toString() + "Healing: " + healingGivenCounter)
         if (showDamageTaken)
-            renderStrings.add(EnumChatFormatting.DARK_RED + "Damage Taken: " + Player.INSTANCE.getDamageTakenCounter());
+            renderStrings.add(EnumChatFormatting.DARK_RED.toString() + "Damage Taken: " + damageTakenCounter)
         if (showHealingReceived)
-            renderStrings.add(EnumChatFormatting.DARK_GREEN + "Healing Received: " + Player.INSTANCE.getHealingReceivedCounter());
-        if (showEnergyGiven && Player.INSTANCE.getSpecialization() == Player.Classes.CRUSADER)
-            renderStrings.add(EnumChatFormatting.YELLOW + "Energy Given: " + Player.INSTANCE.getEnergyGivenCounter());
-        if (showEnergyReceived)
-            renderStrings.add(EnumChatFormatting.YELLOW + "Energy Received: " + Player.INSTANCE.getEnergyReceivedCounter());
-        if (showEnergyLost)
-            renderStrings.add(EnumChatFormatting.YELLOW + "Energy Lost: " + Player.INSTANCE.getEnergyLostCounter());
-        if (showEnergyStolen && Player.INSTANCE.getSpecialization() == Player.Classes.AVENGER)
-            renderStrings.add(EnumChatFormatting.YELLOW + "Energy Stolen: " + Player.INSTANCE.getEnergyStolenCounter());
+            renderStrings.add(EnumChatFormatting.DARK_GREEN.toString() + "Healing Received: " + healingReceivedCounter)
+        if (showEnergyGiven && energyGivenCounter > 0) //specialization === Player.Classes.CRUSADER
+            renderStrings.add(EnumChatFormatting.YELLOW.toString() + "Energy Given: " + energyGivenCounter)
+        if (showEnergyReceived && energyReceivedCounter > 0)
+            renderStrings.add(EnumChatFormatting.YELLOW.toString() + "Energy Received: " + energyReceivedCounter)
+        if (showEnergyLost && energyLostCounter > 0)
+            renderStrings.add(EnumChatFormatting.YELLOW.toString() + "Energy Lost: " + energyLostCounter)
+        if (showEnergyStolen && energyStolenCounter > 0) //specialization === Player.Classes.AVENGER &&
+            renderStrings.add(EnumChatFormatting.YELLOW.toString() + "Energy Stolen: " + energyStolenCounter)
 
-
-        return renderStrings.toArray(new String[0]);
+        return renderStrings.toTypedArray()
     }
 
-    @Override
-    public boolean isVisible() {
-        return GameStateManager.INSTANCE.isIngame();
+    override fun isVisible(): Boolean {
+        return isIngame
     }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
+    override fun isEnabled(): Boolean {
+        return true
+    }
+
+    companion object {
+        @ConfigPropertyBoolean(
+            category = CCategory.HUD,
+            id = "showHealingDone",
+            comment = "Enable or disable the Healing Done counter",
+            def = true
+        )
+        var showHealingDone = false
+
+        @ConfigPropertyBoolean(
+            category = CCategory.HUD,
+            id = "showDamageDone",
+            comment = "Enable or disable the Damage Done counter",
+            def = true
+        )
+        var showDamageDone = false
+
+        @ConfigPropertyBoolean(
+            category = CCategory.HUD,
+            id = "showEnergyReceived",
+            comment = "Enable or disable the Energy Received counter",
+            def = true
+        )
+        var showEnergyReceived = false
+
+        @ConfigPropertyBoolean(
+            category = CCategory.HUD,
+            id = "showHealingReceived",
+            comment = "Enable or disable the Healing Received counter",
+            def = true
+        )
+        var showHealingReceived = false
+
+        @ConfigPropertyBoolean(
+            category = CCategory.HUD,
+            id = "showDamageTaken",
+            comment = "Enable or disable the Damage Taken counter",
+            def = true
+        )
+        var showDamageTaken = false
+
+        @ConfigPropertyBoolean(
+            category = CCategory.HUD,
+            id = "showEnergyGiven",
+            comment = "Enable or disable the Energy Given counter",
+            def = true
+        )
+        var showEnergyGiven = false
+
+        @ConfigPropertyBoolean(
+            category = CCategory.HUD,
+            id = "showEnergyStolen",
+            comment = "Enable or disable the Energy Stolen counter",
+            def = true
+        )
+        var showEnergyStolen = false
+
+        @ConfigPropertyBoolean(
+            category = CCategory.HUD,
+            id = "showEnergyLost",
+            comment = "Enable or disable the Energy Lost counter",
+            def = true
+        )
+        var showEnergyLost = false
     }
 }
