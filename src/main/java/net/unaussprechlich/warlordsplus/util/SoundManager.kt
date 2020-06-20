dd6502766f77180be6bc9ad23a0016f92bb8d361
WM <34175525+ebicep@users.noreply.github.com>
2020-06-19 21:41:49 -0400

    important sound

--- src/main/java/net/unaussprechlich/warlordsplus/util/SoundManager.kt
+++ src/main/java/net/unaussprechlich/warlordsplus/util/SoundManager.kt
@@ -4,13 +4,19 @@ import net.minecraft.client.Minecraft
 import net.unaussprechlich.warlordsplus.WarlordsPlus
 
 enum class Sounds(val soundName: String) {
-    MEME_SUCTION("meme.suction");
+    MEME_SUCTION("meme.suction"),
+    MEME_T1BBC("meme.t1TBBC"),
+    MEME_T1BESTRONG("meme.t1BeStrong"),
+    MEME_T1CHRISTMAS("meme.t1Christmas"),
+    MEME_T1GTFO("meme.t1GTFO"),
+    MEME_T1KS("meme.t1KS"),
+    MEME_T1THANKSFORGOLD("meme.t1ThanksForGold");
 }
 
 object SoundManager {
 
     fun playSound(sound: Sounds) {
-        Minecraft.getMinecraft().thePlayer.playSound(WarlordsPlus.MODID + ":" + sound.soundName, 5f, 1f)
+        Minecraft.getMinecraft().thePlayer.playSound(WarlordsPlus.MODID + ":" + sound.soundName, 10f, 1f)
     }
 
 }
