f64f2be72c22339b6e46b498516765a002f7a8a7
ebicep <wmah314@gmail.com>
2022-10-29 14:18:39 -0400

    test

--- src/main/java/net/unaussprechlich/warlordsplus/commands/TestCommand.kt
+++ src/main/java/net/unaussprechlich/warlordsplus/commands/TestCommand.kt
@@ -3,6 +3,8 @@ package net.unaussprechlich.warlordsplus.commands
 import net.minecraft.client.Minecraft
 import net.minecraft.command.CommandException
 import net.minecraft.command.ICommandSender
+import net.unaussprechlich.warlordsplus.module.modules.qualityoflife.PowerUpTimer
+import net.unaussprechlich.warlordsplus.util.Colors
 
 class TestCommand : AbstractCommand() {
     override fun getCommandName(): String {
@@ -15,8 +17,21 @@ class TestCommand : AbstractCommand() {
 
     @Throws(CommandException::class)
     override fun processCommand(sender: ICommandSender, parameters: Array<String>) {
-        for (i in 0..5000) {
-            Minecraft.getMinecraft().thePlayer.sendChatMessage("" + i)
+//        for (i in 0..5000) {
+//            Minecraft.getMinecraft().thePlayer.sendChatMessage("" + i)
+//        }
+        for (value in PowerUpTimer.powerUps.values) {
+            when {
+                value.color == Colors.GREEN || value.color == Colors.ORANGE -> {
+                    println("new PowerupOption(loc.addXYZ(${value.x}, ${value.y}, ${value.z}), PowerupType.HEALING)")
+                }
+                value.color == Colors.RED -> {
+                    println("new PowerupOption(loc.addXYZ(${value.x}, ${value.y}, ${value.z}), PowerupType.DAMAGE)")
+                }
+                value.color == Colors.YELLOW -> {
+                    println("new PowerupOption(loc.addXYZ(${value.x}, ${value.y}, ${value.z}), PowerupType.SPEED)")
+                }
+            }
         }
     }
 }
